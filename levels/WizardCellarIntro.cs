6076fab7e98f38fa4ae29b67fa710296ce3cf541
Ilari Tommiska <ilari97@hotmail.com>
2024-04-15 20:41:13 +0300

    idklol

--- levels/WizardCellarIntro.cs
+++ levels/WizardCellarIntro.cs
@@ -13,6 +13,15 @@ public partial class WizardCellarIntro : Node2D {
 	[Export]
     public AnimatedSprite2D Pentagrammi;
 
+	[Export]
+	public AudioStreamPlayer TransformationLoop;
+
+	[Export]
+	public AudioStreamPlayer VictoryRoyale;
+
+	[Export]
+	public AudioStreamPlayer DedTheme;
+
 
     public override void _Ready() {
         Animation.Play("kitty_idle");
@@ -20,6 +29,10 @@ public partial class WizardCellarIntro : Node2D {
         Player.IsInCinematic = true;
         Player.Sprite.Visible = false;
 		Pentagrammi.Visible = false;
+
+		Player.ExplocationMusic.Play();
+		Player.CombatMusic.Stop();
+		Player.EnterCombatMusic.Stop();
 
 		GetTree().CreateTimer(2.0f).Timeout += () => {
 			StartIntro();
@@ -48,6 +61,10 @@ public partial class WizardCellarIntro : Node2D {
 	private void FirstDialogueFinished() {
 		Dialogue.DialogueFinished -= FirstDialogueFinished;
 
+
+		Player.ExplocationMusic.Stop();
+		Player.CombatMusic.Stop();
+		Player.EnterCombatMusic.Play();
 		Player.StartEncounter(null); // HACK: null encounter skips level transition
 
 		GetTree().CreateTimer(3.0f).Timeout += () => {
@@ -74,8 +91,13 @@ public partial class WizardCellarIntro : Node2D {
 	private void SecondDialogueFinished() {
 		Dialogue.DialogueFinished -= SecondDialogueFinished;
 
+		Player.ExplocationMusic.Stop();
+		Player.CombatMusic.Stop();
+		Player.EnterCombatMusic.Stop();
+
 		GetTree().CreateTimer(1.5f).Timeout += () => {
 			Animation.Play("transformation");
+			TransformationLoop.Play();
 			Animation.AnimationFinished += TransformationFinished;
 		};
 	}
@@ -84,6 +106,9 @@ public partial class WizardCellarIntro : Node2D {
 		Animation.AnimationFinished -= TransformationFinished;
 		Player.Sprite.Visible = true;
 		Ritual.Visible = false;
+
+		TransformationLoop.Stop();
+		VictoryRoyale.Play();
 
 		Dialogue.DialogueFinished += IntroFinished;
 		Dialogue.Start(WhoIsSpeaking.Paju, new string[] {
@@ -94,5 +119,10 @@ public partial class WizardCellarIntro : Node2D {
 	private void IntroFinished() {
 		Dialogue.DialogueFinished -= IntroFinished;
 		Player.IsInCinematic = false;
+
+		Player.ExplocationMusic.Stop();
+		Player.CombatMusic.Stop();
+		Player.EnterCombatMusic.Stop();
+		DedTheme.Play();
 	}
 }
